92ec75d425dd048505314c0bcab0ae1d5aae01df
rumi <rumi@rumi-no-MacBook-Pro.local>
2013-11-02 18:18:56 +0900

    Initial Commit

---
+++ ASHatenaPhotoClient/ASAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ASAppDelegate.h
+//  ASHatenaPhotoClient
+//
+//  Created by rumi on 2013/11/02.
+//  Copyright (c) 2013年 Mocology. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ASAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ASHatenaPhotoClient/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ASHatenaPhotoClient
+//
+//  Created by rumi on 2013/11/02.
+//  Copyright (c) 2013年 Mocology. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ASAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ASAppDelegate class]));
+    }
+}
